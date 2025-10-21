4a6f95c39c010357f54d210d0eafbe4b627c561d
Akshith <narlakanti.akshith1234@gmail.com>
2025-10-21 15:41:00 +0530

    Fix Render deployment port binding

--- newsapi.py
+++ newsapi.py
@@ -55,6 +55,8 @@ def scrape():
             continue
         break
     return jsonify(lst)
-
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    import os
+    port = int(os.environ.get('PORT', 5002))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
